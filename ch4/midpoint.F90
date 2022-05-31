1da2f5f09f7fd049a1e53b9003acc4f5ea506b39
jphaupt <jphaupt@protonmail.com>
2022-05-31 09:54:48 +0200

    Code cleanup

--- ch4/midpoint.F90
+++ ch4/midpoint.F90
@@ -1,13 +1,5 @@
-
-!    // "pp_defs": {
-!    //     "ENABLE_MPI": "ON"
-!    // },
-    ! "ext_source_dirs": [
-    !     "/home/phaupt/software/openmpi_src/openmpi-4.1.3/ompi/mpi/fortran/use-mpi-f08",
-    !     "/home/phaupt/software/openmpi_src/openmpi-4.1.3/ompi/mpi/fortran/use-mpi-tkr",
-    !     "/home/phaupt/software/openmpi_src/openmpi-4.1.3/ompi/mpi/fortran/base"
-    !     ]
-! TODO universal config file!
+! TODO would be nice to have syntax highlighting for code that is enabled like in C++
+!      - maybe suggest this on the github repo
 program midpoint
     ! exercise 4.11
     ! [x] a. parallelise the code with MPI
@@ -16,8 +8,8 @@ program midpoint
     ! [ ] MPI wrapper so that e.g. timer does something else if serial
     ! this program computes and prints pi using the rectangle (midpoint) rule
     use iso_fortran_env, only: rp => real64
-    use mpi_f08
 #ifdef ENABLE_MPI
+    use mpi_f08
 #endif
     implicit none
     integer, parameter :: MAX_INTERVALS = 1000000
@@ -26,15 +18,8 @@ program midpoint
     ! variables for MPI subroutines
     integer :: ierr, my_rank, nproc
     real(rp) :: time
-    integer :: testvar ! unused if ENABLE_MPI not defined
-    integer :: testvar2
-    ! TODO seems to be the only way to define variables :/
-#define ANOTHER_ONE
 
 #ifdef ENABLE_MPI
-    ! TODO make VS Code recognise preprocessor usage (here, ierr is supposedly unused)
-    ! TODO for some reason VS Code keeps uninstalling Modern Fortran extension
-    ! intellisense not applied here :/
     call MPI_Init(ierr)
     time = - MPI_WTime()
     call MPI_Comm_rank_f08(MPI_COMM_WORLD, my_rank, ierr)
@@ -45,10 +30,6 @@ program midpoint
     my_rank = 0
     nproc = 1
 #endif
-#ifdef ANOTHER_ONE
-    ! TODO delete
-    testvar2 = 2
-#endif
     print*, 'nproc=', nproc
 
     ysum_loc = 0
@@ -57,8 +38,8 @@ program midpoint
         xi = (i+0.5_rp)/MAX_INTERVALS
         ysum_loc = ysum_loc + 4._rp/(1._rp + xi*xi)
     enddo
-    ! NOTE below MPI_DOUBLE_PRECISION works but not MPI_REAL (!!!)
 #ifdef ENABLE_MPI
+    ! NOTE below MPI_DOUBLE_PRECISION works but not MPI_REAL (!!!)
     call MPI_Reduce_f08(ysum_loc, ysum, 1, MPI_DOUBLE_PRECISION, MPI_SUM, 0, MPI_COMM_WORLD, ierror=ierr)
     time = time + MPI_Wtime()
 #endif
